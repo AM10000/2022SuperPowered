fe7c7c26b12690ef102a8c5eafef559c163a2a9a
phanciy2 <tanishipandya@gmail.com>
2022-08-19 12:29:09 -0400

    comments in run 2

--- run2Test.py
+++ run2Test.py
@@ -4,9 +4,13 @@ from time import sleep
 from ev3dev2.motor import MoveTank
 from ev3dev2.sensor import *
 k= Kronos()
- 
+
 k.moveDistance(45, 71)
-k.lfLColorRangeR(20, True, 45, 47,)
+k.moveUntilColorlt(20, 20, 1, 47)
 k.LmediumMotorDegrees(35, 40)
+#done with power plant
 k.spinRobot(20,-20,30)
 k.moveDistance(30, 40)
+#done with hydroelectric dam and collected looped water
+k.spinRobot(-20, 20, -30)
+k.moveDistance(45, 30)
