6152783611a01157fa74d35499c9abd94cf90e50
Ant361 <jipyip567>
2022-10-02 16:53:57 -0400

    tyring the whole thing togther now - run 4

--- run4Test.py
+++ run4Test.py
@@ -3,7 +3,6 @@ from Kronos import Kronos
 from ev3dev2.sensor import INPUT_1, INPUT_2, INPUT_4
 
 k = Kronos()
-'''
 k.moveDistance(30, 30, 20)
 k.lfLTime(15, True, 2)
 k.moveUntilColorlt(40, 40, INPUT_4, 12)
@@ -11,9 +10,7 @@ k.moveDistance(15, 15, 2)
 k.spinRobot(left_power=15,right_power=-15, degrees=45)
 k.moveDistance(30, 30, 15)
 k.moveDistance(30, 30, 12)
-'''
-k.RmediumMotorDegrees(speed=-40, degrees=50)
-'''
+k.RmediumMotorDegrees(speed=40, degrees=70)
 #start of high five shared mission 
 k.spinRobot(left_power=15,right_power=-15, degrees=70)
 k.moveDistance(40, 40, 25)
@@ -25,4 +22,3 @@ k.moveDistance(speedL=50,speedR=50,distanceCM=35)
 k.spinRobot(left_power=0, right_power=30,degrees=160)
 k.moveDistance(speedL=100,speedR=100,distanceCM=100)
 #speed up 
-'''
