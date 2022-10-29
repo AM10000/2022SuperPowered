c3c36806b421a413346ace5dc7952ec2163e64cc
little-meep <amhaskar@outlook.com>
2022-10-29 16:33:48 -0400

    trying to complete this random as heck stuff :)

--- run4Test.py
+++ run4Test.py
@@ -3,19 +3,23 @@ from Kronos import Kronos
 from ev3dev2.sensor import INPUT_1, INPUT_2, INPUT_4
 
 k = Kronos()
-k.moveDistance(30, 30, 20)
+k.moveDistance(30, 30, 25)
 k.lfLTime(15, True, 2)
 k.moveUntilColorlt(40, 40, INPUT_4, 12)
 k.moveDistance(15, 15, 2)
 k.spinRobot(left_power=15,right_power=-15, degrees=45)
-k.moveDistance(30, 30, 15)
-# k.moveDistance(30, 30, 12)
-'''
-k.RmediumMotorDegrees(speed=40, degrees=300)
+k.moveDistance(30, 30, 17)
+#k.moveDistance(30, 30, 12)
+
 #start of high five shared mission 
-k.spinRobot(left_power=15,right_power=-15, degrees=70)
-k.moveDistance(40, 40, 25)
-k.spinRobot(15, 0, 60)
+k.spinRobot(left_power=15,right_power=-15, degrees=60)
+#adjust to the sweet spot for distance for both the units and the water units 
+k.moveDistance(40, 40, 13)
+k.spinRobot(40, 0, 55)
+k.LmediumMotorDegrees(speed=15, degrees=180)
+k.RmediumMotorDegrees(speed=100, degrees=600)
+'''
+k.spinRobot(40, 0, 125)
 k.moveDistance(speedL=-10, speedR=-10, distanceCM=3, block=False)
 k.LmediumMotorDegrees(speed=-10, degrees=100)
 k.LmediumMotorDegrees(speed=-10, degrees=100, block=False)
